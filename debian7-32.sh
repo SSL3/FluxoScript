464860fa6d77e5e8f0d64e858142a99cc0172fed
SSL3 <34892546+SSL3@users.noreply.github.com>
2018-01-06 17:13:54 +0000

    Update debian7-32.sh

--- debian7-32.sh
+++ debian7-32.sh
@@ -77,7 +77,7 @@ wget -O /etc/openvpn/1194.conf "https://github.com/avitjenu/khathuluthvpn/raw/ma
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://github.com/avitjenu/khathuluthvpn/blob/master/iptables.up.rules"
+wget -O /etc/iptables.up.rules "https://github.com/avitjenu/khathuluthvpn/raw/master/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
